43e423d1d83ab6622321053c4bf448d0166443f4
Seddiki101 <seddiki915@mail.com>
2022-06-23 14:25:07 +0100

    improvements

--- proto3/proj/scr/converge.ps1
+++ proto3/proj/scr/converge.ps1
@@ -1,5 +1,5 @@
 #if pdfs we extracting them and ocring all extracted images  dow-> input / upp -> output
-$tab0 = Get-ChildItem -Path "..\dow\*.pdf" -Name
+$tab0 = Get-ChildItem -Path "dwn\*.pdf" -Name
 $i=0
 $j=0;
 foreach ($item in $tab0) {$i++}
@@ -10,44 +10,46 @@ if($i -gt 0) {
 
 
 
-$tab1 = Get-ChildItem -Path "..\dow\*.png" -Name
-$tab2 = @(Get-ChildItem -Path '..\dow\*.jpg' -Name )
-$tab3 = @(Get-ChildItem -Path '..\dow\*.jpeg' -Name )
+
+$tab1 = Get-ChildItem -Path "dwn\*.png" -Name
+$tab2 = @(Get-ChildItem -Path 'dwn\*.jpg' -Name )
+$tab3 = @(Get-ChildItem -Path 'dwn\*.jpeg' -Name )
 #preprocess aka pdfs
-$tab4 = Get-ChildItem -Path "..\pre\*.jpg" -Name
+$tab4 = Get-ChildItem -Path "prep\*.jpg" -Name
 
 #else we ocring direclty-------------------------
 	
 foreach ($item in $tab1){
-	$item2='..\dow\'+$item
-	$path='..\upp\'+"result"
+	$item2='dwn\'+$item
+	$path='upp\'+"result"
 	tesseract -l ara $item2 $path | cmd
 		}
 
 foreach ($item in $tab2){
-	$item2='..\dow\'+$item
-	$path='..\upp\'+"result"
+	$item2='dwn\'+$item
+	$path='upp\'+"result"
 	tesseract -l ara $item2 $path | cmd
 		}
 
 
 foreach ($item in $tab3){
-	$item2='..\dow\'+$item
-	$path='..\upp\'+"result"
+	$item2='dwn\'+$item
+	$path='upp\'+"result"
 	tesseract -l ara $item2 $path | cmd
 		}
 
 
 foreach ($item in $tab4){
-	$item2='..\pre\'+$item
-	$path='..\upp\'+$item
+	$item2='prep\'+$item
+	$path='upp\'+$item
 	tesseract -l ara $item2 $path | cmd
 		}
 		
-		
-		
-		
+				
 #-------------------------------Joining txt files    !may be ordering problem -> more work for future me 
-if($j -eq 1) { Get-Content upp\page*.txt | Set-Content ..\upp\result.txt
-Remove-Item ..\upp\page* }
+if($j -eq 1) { Get-Content upp\page*.txt | Set-Content upp\result.txt
+Remove-Item upp\page* }
 $j=0;
+
+
+Start-Sleep -s 5
